ce976aa1c520b08231ac21d21a8a4de00f2af3b7
Adam <hanada-2@student.ltu.se>
2023-12-08 14:49:32 +0100

    finished test

--- test/recipe_details_test.dart
+++ test/recipe_details_test.dart
@@ -4,26 +4,31 @@ import 'package:simple_suppers/screens/recipe_details.dart';
 
 void main() {
   testWidgets('RecipeDetails widget test', (WidgetTester tester) async {
-    // Create a test widget
     await tester.pumpWidget(
       MaterialApp(
         home: RecipeDetails(recipeId: 1),
       ),
     );
 
-    // Verify that the app bar title is displayed correctly
-    expect(find.text('SimpleSuppers'), findsOneWidget);
-
     // Verify that the loading indicator is displayed initially
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
 
-    // Simulate the completion of the future that fetches the recipe
+    // Wait for screen to load
     await tester.pump(Duration(seconds: 2));
 
-    // Verify that the loading indicator is no longer displayed
+    // App bar title is displayed correctly
+    expect(find.text('SimpleSuppers'), findsOneWidget);
+
+    // Loading indicator is no longer visible
     expect(find.byType(CircularProgressIndicator), findsNothing);
 
-    // Verify that the recipe details are displayed correctly
+    // Verify that the recipe details are displayed
     expect(find.text('Recipe not found'), findsNothing);
+
+    // Placeholder picture is not displayed
+    expect(
+        find.text(
+            "https://kotivara.se/wp-content/uploads/2023/02/Pizza-scaled-1-1024x683.jpg"),
+        findsNothing);
   });
 }
